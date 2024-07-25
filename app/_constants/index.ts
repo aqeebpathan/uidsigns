cb1ab8bd0d0b58a3c77e79fbaec7b9b26e019f65
Mohmmed Aqeeb <developer.aqeeb@gmail.com>
2024-07-25 20:59:04 +0530

    Add constants

---
+++ app/_constants/index.ts
@@ -0,0 +1,246 @@
+export const navLinks = [
+  { label: "About", url: "/about" },
+  { label: "Collections", url: "/collections" },
+  { label: "Submit", url: "/submit" },
+];
+
+export const termsOfService = {
+  title: "Terms of Service",
+  sections: [
+    {
+      content:
+        "Welcome to UiDsigns and thanks for using our platform. These Terms of Service govern your use of our website as a platform for sharing and exploring UI/UX designs sourced from various platforms.",
+    },
+    {
+      heading: "Agreement to Terms",
+      content:
+        "By accessing or using UiDsigns, you agree to be bound by these Terms of Service, all applicable laws, and regulations. If you do not agree with any of these terms, you are prohibited from using or accessing this site.",
+    },
+    {
+      heading: "Use of Content",
+      content: [
+        {
+          subheading: "Designs",
+          text: "UiDsigns provides a platform to view and explore UI/UX designs sourced from third-party platforms such as Dribbble and Behance. You may view, share, and bookmark designs for personal inspiration and reference.",
+        },
+        {
+          subheading: "Attribution",
+          text: "When sharing or using designs from UiDsigns, you agree to attribute the original creator and adhere to any licensing terms specified by the creator on the original platform.",
+        },
+        // {
+        //   subheading: "Your Designs",
+        //   text: "You can submit your UI/UX designs to UiDsigns for potential feature on the website. By submitting your design, you grant UiDsigns permission to display and promote your design on the platform.",
+        // },
+        // {
+        //   subheading: "Displaying Submitted Websites",
+        //   text: "UiDsigns reserves the right to feature and display submitted UI/UX designs on its platform. The decision to feature a design is at the discretion of UiDsigns.",
+        // },
+      ],
+    },
+    {
+      heading: "Your Designs",
+      content: [
+        {
+          text: "You can submit your UI/UX designs to UiDsigns for potential feature on the website. By submitting your design, you grant UiDsigns permission to display and promote your design on the platform.",
+        },
+        {
+          subheading: "Displaying Submitted Websites",
+          text: "UiDsigns reserves the right to feature and display submitted UI/UX designs on its platform. The decision to feature a design is at the discretion of UiDsigns.",
+        },
+      ],
+    },
+    {
+      heading: "Intellectual Property",
+      content: [
+        {
+          subheading: "Ownership",
+          text: "All UI/UX designs featured on UiDsigns remain the property of their respective creators. UiDsigns does not claim ownership over any designs shared on the platform.",
+        },
+        {
+          subheading: "Copyright Infringement",
+          text: "UiDsigns respects intellectual property rights and expects users to do the same. If you believe that your work has been copied in a way that constitutes copyright infringement, please contact us.",
+        },
+      ],
+    },
+    {
+      heading: "User Responsibilities",
+      content: [
+        {
+          subheading: "Compliance",
+          text: "You agree to use UiDsigns in compliance with all applicable laws, regulations, and these Terms of Service.",
+        },
+        {
+          subheading: "Prohibited Actions",
+          text: "You may not modify, adapt, reproduce, distribute, translate, create derivative works, or reverse engineer any portion of UiDsigns or its content.",
+        },
+      ],
+    },
+    {
+      heading: "Data Collection and Privacy",
+      content:
+        "UiDsigns may use cookies to enhance your browsing experience. By using UiDsigns, you consent to the use of cookies in accordance with our Privacy Policy.",
+    },
+    {
+      heading: "Liability and Disclaimers",
+      content: [
+        {
+          subheading: "No Warranty",
+          text: "UiDsigns provides the platform 'as is' without any warranties, express or implied. We do not warrant the accuracy, completeness, or reliability of any content on the platform.",
+        },
+        {
+          subheading: "Limitation of Liability",
+          text: "In no event shall UiDsigns or its affiliates be liable for any indirect, incidental, special, consequential, or punitive damages, including but not limited to loss of profits, data, use, or other intangible losses.",
+        },
+      ],
+    },
+    {
+      heading: "Modifications",
+      content:
+        "UiDsigns reserves the right to modify or replace these Terms of Service at any time. It is your responsibility to check these Terms of Service periodically for changes. Your continued use of UiDsigns following the posting of any changes to these Terms of Service constitutes acceptance of those changes.",
+    },
+    {
+      heading: "Contact Us",
+      content:
+        "If you have any questions about these Terms of Service, please contact us at contact@uidsigns.com.",
+    },
+  ],
+};
+
+export const privacyPolicy = {
+  title: "Privacy Policy",
+  sections: [
+    {
+      content: [
+        {
+          subheading: "",
+          text: "Welcome to UiDsigns! This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website.",
+        },
+      ],
+    },
+    {
+      heading: "Information We Collect",
+      content: [
+        {
+          subheading: "Personal Information",
+          text: "We do not collect any personal information directly from users. All design submissions are made via email and handled manually.",
+        },
+        {
+          subheading: "Non-Personal Information",
+          text: "We may collect non-personal information about your visit to our website, such as your IP address, browser type, and access times.",
+        },
+      ],
+    },
+    {
+      heading: "Use of Information",
+      content: [
+        {
+          subheading: "",
+          text: "We use the information we collect to operate and maintain our website, improve our services, and analyze how users interact with our site.",
+        },
+        {
+          subheading: "",
+          text: "We do not use your personal information for marketing purposes and do not sell, trade, or otherwise transfer your information to outside parties.",
+        },
+      ],
+    },
+    {
+      heading: "Cookies",
+      content: [
+        {
+          subheading: "",
+          text: "We use cookies to enhance your browsing experience. By using UiDsigns, you consent to the use of cookies in accordance with our Privacy Policy.",
+        },
+      ],
+    },
+    {
+      heading: "Third-Party Services",
+      content: [
+        {
+          subheading: "",
+          text: "We may use third-party services to analyze non-personal data and to help maintain our website. These services may use cookies or other technologies to perform their services.",
+        },
+      ],
+    },
+    {
+      heading: "Data Security",
+      content: [
+        {
+          subheading: "",
+          text: "We implement a variety of security measures to maintain the safety of your personal information. However, no method of transmission over the internet, or method of electronic storage, is 100% secure.",
+        },
+      ],
+    },
+    {
+      heading: "Links to Other Sites ",
+      content: [
+        {
+          subheading: "External Links",
+          text: "Our website may contain links to external sites that are not operated by us. We have no control over the content and practices of these sites and cannot accept responsibility or liability for their respective privacy policies",
+        },
+      ],
+    },
+    {
+      heading: "Changes to This Privacy Policy",
+      content: [
+        {
+          subheading: "",
+          text: "UiDsigns reserves the right to modify this Privacy Policy at any time. It is your responsibility to check this Privacy Policy periodically for changes. Your continued use of UiDsigns following the posting of any changes to this Privacy Policy constitutes acceptance of those changes.",
+        },
+      ],
+    },
+    {
+      heading: "Contact Us",
+      content: [
+        {
+          subheading: "",
+          text: "If you have any questions about this Privacy Policy, please contact us at contact@uidsigns.com.",
+        },
+      ],
+    },
+  ],
+};
+
+export const aboutContent = {
+  title: "About UiDsigns",
+  tagline: "Discover, Inspire, Create",
+  introduction:
+    "At UiDsigns, we’re all about celebrating the brilliance of UI/UX design. Our platform is dedicated to showcasing a handpicked selection of remarkable designs from around the globe. Whether you're a designer looking for fresh ideas or a design enthusiast eager to explore the latest trends, UiDsigns is here to inspire you.",
+
+  sections: [
+    {
+      type: "vision",
+      heading: "Our Vision",
+      text: "We envision a world where exceptional design is accessible and appreciated by all. By curating top-notch UI/UX designs from sources like Dribbble and Behance, we aim to create a space where creativity thrives and design excellence is recognized.",
+    },
+    {
+      type: "offerings",
+      heading: "What We Offer",
+      items: [
+        {
+          title: "Selected Design Collections",
+          description:
+            "Discover carefully chosen UI/UX designs that exemplify excellence and innovation. Each piece in our collection is handpicked to ensure it meets the highest standards of quality and creativity.",
+        },
+
+        {
+          title: "Submit Your Design",
+          description:
+            "Are you a designer with work that stands out? Submit your design to us for a chance to be featured on uidsigns. We welcome submissions from designers of all levels and backgrounds.",
+        },
+        {
+          title: "Inspiration and Trends",
+          description:
+            "Stay updated with the latest trends and insights in the UI/UX design world. Our blog and articles section offers valuable content that can help you stay ahead in the ever-evolving field of design.",
+        },
+        {
+          title: "Designer Spotlight (Coming Soon)",
+          description:
+            "We feature top designers and their portfolios, providing a platform for them to showcase their work and share their creative journeys. Learn from the best and get inspired by their stories and insights.",
+        },
+      ],
+    },
+  ],
+
+  closing:
+    "for visiting uidsigns. We hope our collections inspire you and help you create designs that define excellence!",
+};
