0a5853937f35bd3f19cca974fe322dc483970d4e
Mohmmed Aqeeb <developer.aqeeb@gmail.com>
2024-07-26 15:44:57 +0530

    Fix 'background gradient' bug

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <main>
       <div className="w-full bg-black bg-grid-white/[0.2] relative">
         {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <Hero />
       </div>
       <FeatureDesigns />
