e44449724709fdb4afe4a4f0ddb7595886091acc
Mohmmed Aqeeb <developer.aqeeb@gmail.com>
2024-07-26 11:52:56 +0530

    Fix background color bug

--- app/_components/LegalDocumentSection.tsx
+++ app/_components/LegalDocumentSection.tsx
@@ -23,7 +23,7 @@ const LegalDocumentSection: React.FC<LegalDocumentSectionProps> = ({
 }) => {
   return (
     <section>
-      <div className="w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative pt-32 pb-20 fade-out">
+      <div className="w-full bg-black bg-grid-white/[0.2] relative pt-32 pb-20 fade-out">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import FeatureDesigns from "./_components/FeatureDesigns";
 const Home = () => {
   return (
     <main>
-      <div className="w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative">
+      <div className="w-full bg-black bg-grid-white/[0.2] relative">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <Hero />
