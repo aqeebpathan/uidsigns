bdaf9c8ea1c8d1f41c34be3798534e28b0c9aa34
Mohmmed Aqeeb <developer.aqeeb@gmail.com>
2024-08-23 20:36:23 +0530

    Add images 'hostnames'

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dribbble.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.behance.net",
+      },
+      {
+        protocol: "https",
+        hostname: "gyiktpapyfjjwgcfuhik.supabase.co",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
