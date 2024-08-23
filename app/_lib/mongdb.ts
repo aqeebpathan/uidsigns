492d3364354f0730927f852a27552538e55c3403
Mohmmed Aqeeb <developer.aqeeb@gmail.com>
2024-08-23 20:50:06 +0530

    Add new 'MongoDB connection' function

---
+++ app/_lib/mongdb.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectToMongoDB = async () => {
+  const mongoURI = process.env.MONGODB_URI;
+
+  if (!mongoURI) {
+    console.error("MONGODB_URI environment variable is not set.");
+    process.exit(1);
+  }
+
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to MONGODB🥭");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+export default connectToMongoDB;
