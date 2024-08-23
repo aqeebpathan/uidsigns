9cdaf91992e6d123edce64621ea97afa3d8a1c6a
Mohmmed Aqeeb <developer.aqeeb@gmail.com>
2024-08-23 20:28:13 +0530

    Add 'DesignCard' and Fetch data

--- app/_components/FeatureDesigns.tsx
+++ app/_components/FeatureDesigns.tsx
@@ -1,39 +1,48 @@
-import Image from "next/image";
+interface Design {
+  title: string;
+  imageUrl: string;
+  designerName: string;
+  sourceUrl: string;
+  designerUrl: string;
+}
+
+export async function fetchDesigns(): Promise<Design[]> {
+  try {
+    const res = await fetch("http://localhost:3000/api/designs");
+
+    const data = await res.json();
+
+    return data.designs;
+  } catch (error) {
+    console.log("Error fetching designs:", error);
+    return [];
+  }
+}
+
 import Link from "next/link";
+import DesignCard from "./DesignCard";
+import { title } from "process";
 
-const featureDesigns = [
-  { src: "/ui.png", designerName: "sarah" },
-  { src: "/ui.png", designerName: "sarah" },
-  { src: "/ui.png", designerName: "sarah" },
-  { src: "/ui.png", designerName: "sarah" },
-  { src: "/ui.png", designerName: "sarah" },
-  { src: "/ui.png", designerName: "sarah" },
-];
+const FeatureDesigns = async () => {
+  const designs = await fetchDesigns();
+  console.log(designs);
 
-const FeatureDesigns = () => {
   return (
     <section className=" bg-black">
       <div className="max-w-7xl mx-auto px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
-          {featureDesigns.map(({ src, designerName }, i) => (
-            <div className="group" key={i}>
-              <div className="shadow-md rounded-md h-[300px] overflow-hidden border border-[#3d3d3d] hover:border-[#888888] cursor-pointer transition">
-                <Image
-                  src={src}
-                  alt="UI Design"
-                  width={400}
-                  height={300}
-                  className="w-full object-contain"
-                />
-              </div>
-              <p className="my-3 text-[#ADADAD] text-[17px]">
-                Designed by{" "}
-                <span className="text-white">
-                  <Link href="#">{designerName}</Link>
-                </span>
-              </p>
-            </div>
-          ))}
+          {designs.map(
+            ({ imageUrl, designerName, sourceUrl, designerUrl }, i) => (
+              <DesignCard
+                title={title}
+                imageUrl={imageUrl}
+                designerName={designerName}
+                sourceUrl={sourceUrl}
+                designerUrl={designerUrl}
+                key={i}
+              />
+            )
+          )}
         </div>
 
         <div className="flex justify-center items-center py-10">
