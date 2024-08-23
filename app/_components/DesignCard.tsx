0ed8406db0c1de92fb654278fa912c0c51968abd
Mohmmed Aqeeb <developer.aqeeb@gmail.com>
2024-08-23 20:29:45 +0530

    Add 'DesignCard' component

---
+++ app/_components/DesignCard.tsx
@@ -0,0 +1,52 @@
+interface DesignCardProps {
+  title: string;
+  imageUrl: string;
+  designerName: string;
+  sourceUrl: string;
+  designerUrl: string;
+}
+
+import Image from "next/image";
+import Link from "next/link";
+import { HiOutlineExternalLink } from "react-icons/hi";
+
+const DesignCard: React.FC<DesignCardProps> = ({
+  title,
+  imageUrl,
+  designerName,
+  sourceUrl,
+  designerUrl,
+}) => {
+  return (
+    <div className="group relative">
+      <Link href={sourceUrl} target="_blank">
+        <div className="shadow-md rounded-md h-[300px] overflow-hidden border border-[#3d3d3d] hover:border-[#888888] cursor-pointer transition">
+          <Image
+            src={imageUrl}
+            alt={title}
+            width={400}
+            height={300}
+            className="w-full object-contain"
+          />
+        </div>
+      </Link>
+      <p className="my-3 text-[#ADADAD] text-[17px]">
+        Designed by{" "}
+        <span className="text-white">
+          <Link href={designerUrl} target="_blank">
+            {designerName}
+          </Link>
+        </span>
+      </p>
+
+      <Link
+        href={sourceUrl}
+        className="bg-neutral-100 text-black/90 absolute top-3 right-3 p-1.5 rounded-full opacity-0 group-hover:opacity-100 hover:bg-neutral-900 hover:text-neutral-100 transition"
+      >
+        <HiOutlineExternalLink />
+      </Link>
+    </div>
+  );
+};
+
+export default DesignCard;
