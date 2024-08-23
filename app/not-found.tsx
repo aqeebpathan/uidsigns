ac560867f8894009d5f882811f19605694a0aaaa
Mohmmed Aqeeb <developer.aqeeb@gmail.com>
2024-08-24 00:05:37 +0530

    Fixed background bug

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ const notFound = () => {
   return (
     <section className="w-full h-screen dark:bg-black bg-white text-[#EBEBEB] dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative">
       {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="flex flex-col justify-center items-center h-full">
         <h1 className="text-7xl sm:text-9xl">404</h1>
         <p className="text-2xl mb-2">Page Not Found</p>
