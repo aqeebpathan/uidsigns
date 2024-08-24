b3c64e332c10a0a7c1c2d91d52dff5c1ada44770
Mohmmed Aqeeb <developer.aqeeb@gmail.com>
2024-08-24 11:19:13 +0530

    Fix bg gradient

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const notFound = () => {
   return (
-    <section className="w-full h-screen dark:bg-black bg-white text-[#EBEBEB] dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative">
+    <section className="w-full h-screen bg-black  text-[#EBEBEB] bg-grid-white/[0.2] relative">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="flex flex-col justify-center items-center h-full">
