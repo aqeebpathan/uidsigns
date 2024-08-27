899e9ef08d4e5ad7246f596784ffc2ac28dec9fc
aqeeb <realaqeeb@gmail.com>
2024-08-27 17:06:07 +0530

    Add new 'DesignList' component

---
+++ app/_components/DesignList.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import DesignCard from "./DesignCard";
+
+interface Design {
+  _id: string;
+  uiID: string;
+  title: string;
+  imageUrl: string;
+  designerName: string;
+  designerUrl: string;
+  featured: boolean;
+  category: string;
+  views: number;
+  createdAt: string;
+  updatedAt: string;
+  sourceUrl: string;
+}
+
+interface DesignListProps {
+  initialDesigns: Design[];
+}
+
+const DesignList = ({ initialDesigns }: DesignListProps) => {
+  const [designs, setDesigns] = useState(initialDesigns);
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  const [lastId, setLastId] = useState<string | null>(
+    initialDesigns[initialDesigns.length - 1]?._id || null
+  );
+
+  console.log(designs);
+
+  const fetchMoreDesigns = async () => {
+    try {
+      const response = await fetch(`/api/designs?limit=12&lastId=${lastId}`);
+      const { designs: newDesigns } = await response.json();
+
+      if (newDesigns.length === 0) {
+        setHasMore(false);
+      } else {
+        setDesigns((prevDesigns) => [...prevDesigns, ...newDesigns]);
+        setLastId(newDesigns[newDesigns.length - 1]._id);
+      }
+    } catch (error) {
+      console.error("Error fetching more designs:", error);
+      setHasMore(false);
+    }
+  };
+
+  return (
+    <InfiniteScroll
+      dataLength={designs.length}
+      next={fetchMoreDesigns}
+      hasMore={hasMore}
+      loader={<h4>Loading...</h4>}
+      endMessage={<p>No more designs to show.</p>}
+      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-12"
+    >
+      {designs.map((design) => (
+        <DesignCard
+          key={design._id}
+          title={design.title}
+          imageUrl={design.imageUrl}
+          designerName={design.designerName}
+          designerUrl={design.designerUrl}
+          sourceUrl={design.sourceUrl}
+        />
+      ))}
+    </InfiniteScroll>
+  );
+};
+
+export default DesignList;
