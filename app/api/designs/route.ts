5b13e6c1cf9452d478a678355bd59836c15d1511
aqeeb <realaqeeb@gmail.com>
2024-08-27 22:10:07 +0530

    Update GET route for 'Randomized Feed Generation'

--- app/api/designs/route.ts
+++ app/api/designs/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 import connectToMongoDB from "@app/_lib/mongdb";
 import Design from "@app/_models/designs";
-import mongoose from "mongoose";
 
 export async function POST(req: NextRequest) {
   try {
@@ -68,15 +67,12 @@ export async function GET(req: NextRequest) {
     await connectToMongoDB();
 
     const url = new URL(req.url);
-    const limit = parseInt(url.searchParams.get("limit") || "12", 10);
-    const lastId = url.searchParams.get("lastId"); // ID of the last fetched design
+    const limit = parseInt(url.searchParams.get("limit") || "8", 10);
 
-    const query: any = {};
-    if (lastId) {
-      query._id = { $gt: new mongoose.Types.ObjectId(lastId) }; // Fetch designs after the last ID
-    }
-
-    const designs = await Design.find(query).limit(limit).exec();
+    // Random sampling
+    const designs = await Design.aggregate([
+      { $sample: { size: limit + 0 } }, // Fetch extra to account for potential duplicates
+    ]).exec();
 
     return NextResponse.json({ designs }, { status: 200 });
   } catch (error) {
