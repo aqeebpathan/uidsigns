42859f8f1d6d4d9834f2564749609353ae4cf512
aqeeb <realaqeeb@gmail.com>
2024-08-28 15:57:15 +0530

    Update 'Collection page' GET searchParams and pass it to '<DesignList />'

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -2,19 +2,24 @@ import DesignList from "@app/_components/DesignList";
 import SearchDesignInput from "@app/_components/SearchDesignInput";
 
 async function fetchInitialDesigns() {
-  const res = await fetch(`${process.env.BASE_URL}/api/designs?limit=6`);
+  const res = await fetch(`${process.env.BASE_URL}/api/designs?limit=12`);
   const data = await res.json();
   return data.designs;
 }
 
-const page = async () => {
+const page = async ({
+  searchParams,
+}: {
+  searchParams: { search?: string };
+}) => {
   const initialDesigns = await fetchInitialDesigns();
+  const query = searchParams.search || "";
 
   return (
     <section className="pt-40 pb-24">
       <div className="max-w-7xl mx-auto relative px-4">
         <SearchDesignInput />
-        <DesignList initialDesigns={initialDesigns} />
+        <DesignList initialDesigns={initialDesigns} searchQuery={query} />
       </div>
     </section>
   );
