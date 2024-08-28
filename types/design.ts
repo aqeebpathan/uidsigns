b47d4fa2bfcd9e187109ae5cc94059fc7bdaa034
aqeeb <realaqeeb@gmail.com>
2024-08-28 16:00:49 +0530

    Add seperate 'Design Interface'

---
+++ types/design.ts
@@ -0,0 +1,14 @@
+export interface Design {
+  _id: string;
+  uiID: string;
+  title: string;
+  imageUrl: string;
+  designerName: string;
+  designerUrl: string;
+  featured: boolean;
+  category: string;
+  views: number;
+  createdAt: string;
+  updatedAt: string;
+  sourceUrl: string;
+}
